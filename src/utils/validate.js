404e067c93140970cba2cadb55124dfa11e01dab
Aneesh-Khanna <akkakk1973@gmail.com>
2025-10-20 18:05:03 +0530

    changed validation error message and redeployed final version

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -5,8 +5,8 @@ export const checkValidData = (email, password) => {
   const isPasswordValid =
     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
 
-  if (!isEmailValid) return "Email ID is not valid";
-  if (!isPasswordValid) return "Password is not valid";
+  if (!isEmailValid) return "Email is not valid. Please enter a valid format like name@example.com";
+  if (!isPasswordValid) return "Invalid Password, It must include uppercase, lowercase, a number and be at least 8 characters";
 
   return null;
 };
